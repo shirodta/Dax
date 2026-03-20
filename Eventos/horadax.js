f2a0d200b076f4d54c19bbdfc1f1e631f17d0cb6
shirodta <107359778+shirodta@users.noreply.github.com>
2026-03-20 12:48:10 -0500

    Update horadax.js

--- Eventos/horadax.js
+++ Eventos/horadax.js
@@ -1,1 +1,23 @@
+function actualizarFechaHora(){
+const ahora = new Date();
+// Días y meses abreviados
+const dias = ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"];
+const meses = ["Ene", "Feb", "Mar", "Abr", "Mayo", "Jun", "Jul", "Ago", "Sept", "Oct", "Nov", "Dic"];
+const dia = dias[ahora.getDay()];
+const fecha = ahora.getDate().toString().padStart(2,'0');
+const mes = meses[ahora.getMonth()];
+const anio = ahora.getFullYear();
+// Formato final
+const fechaFinal = `${dia}, ${fecha}. ${mes} ${anio}`;
+// Hora
+const hora = ahora.toLocaleTimeString("es-CO",{
+timeZone:"America/Bogota",
+hour12:false,
+hour:"2-digit",
+minute:"2-digit",
+second:"2-digit"});
+document.getElementById("fecha").textContent = fechaFinal;
+document.getElementById("hora").textContent = hora;}
+setInterval(actualizarFechaHora,1000);
+actualizarFechaHora();
 
